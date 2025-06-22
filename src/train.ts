14c32b79b5cac9e5ec4228752c794898e49c003a
Tony <diyorbeksolijonov27@gmail.com>
2025-06-22 10:56:18 +0900

    Task H has been done

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,59 @@
 console.log("This is the train.ts file");
+console.log('Bugungi MIT-Task natijasi: ');
+
+
+/*TASK H: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+ faqat positive qiymatlarni olib string holatda return qilsin
+
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"*/
+
+//Solution:
+
+function getPositive(arr: number[]) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        console.error("Iltimos, array kiriting.");
+        return false; 
+    }
+    let positiveValues: number[] = arr.filter(num => num > 0);
+    return positiveValues.join('');
+}
+console.log(getPositive([-9, 25, 2])); 
+
+
+
+
+
+
+
+/*TASK G:
+
+Yagona parametrga ega function tuzing.
+Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.*/
+
+/*function getHighestIndex(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        console.error("Iltimos, array kiriting.");
+        return false; 
+    }
+
+    let highestValue = arr[0];
+    let highestIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > highestValue) {
+            highestValue = arr[i];
+            highestIndex = i;
+        }
+    }
+
+    return highestIndex;
+}
+
+console.log(getHighestIndex([12])); */
