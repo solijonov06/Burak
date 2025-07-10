f3153fdec1bc723381f67a54914da0f6ea543289
Tony <diyorbeksolijonov27@gmail.com>
2025-07-10 19:56:35 +0900

    feat:upload files to server

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router(); 
 import restarauntController from "./controllers/restaraunt.controller"
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /*Restarauunt */
 routerAdmin.get("/", restarauntController.goHome);
@@ -10,7 +11,7 @@ routerAdmin
     .post("/login", restarauntController.processLogin);
 routerAdmin
     .get("/signup", restarauntController.getSignup)
-    .post("/signup", restarauntController.processSignup);
+    .post("/signup", makeUploader("members").single("memberImage"),restarauntController.processSignup);
 routerAdmin.get("/logout", restarauntController.logout);
 routerAdmin.get("/check-me", restarauntController.checkAuthSesssion);
 
@@ -22,6 +23,8 @@ productController.getAllProducts
 );
 routerAdmin.post("/product/create",
     restarauntController.verifyRestaraunt,
+    // uploadProductImage.single("productImage"),
+    makeUploader("products").single("productImage"),
      productController.createNewProduct
 
 );
